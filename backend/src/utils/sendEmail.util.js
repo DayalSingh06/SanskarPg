6001f8aafcac56f408bce70563bf338b773d2baa
DayalSingh06 <dayalsingh98191@gmail.com>
2026-05-20 17:41:22 +0530

    fix gmail ipv4 issue

--- backend/src/utils/sendEmail.util.js
+++ backend/src/utils/sendEmail.util.js
@@ -1,16 +1,32 @@
 import nodemailer from "nodemailer";
+import dns from "dns";
+
+// FORCE IPV4
+dns.setDefaultResultOrder("ipv4first");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
 
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+
+  family: 4, // IMPORTANT
+
+  tls: {
+    rejectUnauthorized: false,
+  },
 });
 
 const sendEmail = async ({ to, subject, html }) => {
   try {
+    await transporter.verify();
+
+    console.log("SMTP READY");
+
     const info = await transporter.sendMail({
       from: `"Skool" <${process.env.EMAIL_USER}>`,
       to,
@@ -20,11 +36,17 @@ const sendEmail = async ({ to, subject, html }) => {
 
     console.log("✅ Email Sent:", info.response);
 
-    return info;
+    return {
+      success: true,
+      info,
+    };
   } catch (error) {
     console.log("EMAIL SEND ERROR:", error);
 
-    throw error;
+    return {
+      success: false,
+      error,
+    };
   }
 };
 
