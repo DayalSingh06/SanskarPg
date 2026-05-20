1eba995b143933c5ddead2ef4132a402bf5a377a
DayalSingh06 <dayalsingh98191@gmail.com>
2026-05-20 16:51:43 +0530

    fixed cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://sanskar-web.vercel.app"],
+    origin: ["http://localhost:5173", "https://sanskar-pg.vercel.app"],
     credentials: true,
   }),
 );
