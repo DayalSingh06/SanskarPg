701b0fcd5c586123097253bc0dc7277d83f09cb1
DayalSingh06 <dayalsingh98191@gmail.com>
2026-05-20 19:30:33 +0530

    gmail update

--- backend/src/utils/sendEmail.util.js
+++ backend/src/utils/sendEmail.util.js
@@ -4,23 +4,36 @@ import dns from "dns";
 // FORCE IPV4
 dns.setDefaultResultOrder("ipv4first");
 
+// const transporter = nodemailer.createTransport({
+//   host: "smtp.gmail.com",
+//   port: 587,
+//   secure: false,
+
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASS,
+//   },
+
+//   family: 4, // IMPORTANT
+
+//   tls: {
+//     rejectUnauthorized: false,
+//   },
+// });
+
 const transporter = nodemailer.createTransport({
+
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
 
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-
-  family: 4, // IMPORTANT
-
-  tls: {
-    rejectUnauthorized: false,
-  },
 });
 
+ 
 const sendEmail = async ({ to, subject, html }) => {
   try {
     await transporter.verify();
